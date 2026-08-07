e9158ec686874751668752f22dfb71e5d6655fb0
besnovatyj <metr31@yandex.ru>
2026-08-07 17:02:49 +0500

    drop: outdated

--- src/config/config.php
+++ src/config/config.php
@@ -8,7 +8,5 @@ return [
     'id' => 'Actors',
     'params' => [
         'iconClass' => 'bi bi-person-square',
-
-        'directories' => true, // Если для работы модуля необходимы директории для статики
     ],
 ];
